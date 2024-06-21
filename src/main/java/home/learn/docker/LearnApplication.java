cc35b52b163440e7973694932d2298c4ed011860
nima <linux.vps.vpn@gmail.com>
2024-06-21 12:49:27 +0330

    init

--- src/main/java/home/learn/docker/LearnApplication.java
+++ src/main/java/home/learn/docker/LearnApplication.java
@@ -2,6 +2,8 @@ package home.learn.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearnApplication {
@@ -10,4 +12,11 @@ public class LearnApplication {
         SpringApplication.run(LearnApplication.class, args);
     }
 
+    @RestController
+    class HelloController {
+        @GetMapping("/")
+        public String hello() {
+            return "Hello, World!";
+        }
+    }
 }
