20401c8edff8a912a47e6a2813ea1a080c0ec9a8
nima <linux.vps.vpn@gmail.com>
2024-06-21 12:45:18 +0330

    init

---
+++ src/main/java/home/learn/docker/LearnApplication.java
@@ -0,0 +1,13 @@
+package home.learn.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/home/learn/docker/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package home.learn.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
